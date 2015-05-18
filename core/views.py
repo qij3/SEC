eed55db70f8dc674810a0bc1bc6cae95c65cee79
qjin <qjin@mingyuedeMacBook-Air.local>
2015-05-17 21:19:45 -0700

    Updating landing page template view

--- core/views.py
+++ core/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import TemplateView
+
+from django.views.generic import TemplateView
+
+class LandingView(TemplateView):
+    template_name = 'base/index.html'
      
