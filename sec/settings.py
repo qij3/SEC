eff962fae56d5172c623eba976622475efea554b
qjin <qjin@mingyuedeMacBook-Air.local>
2015-05-18 22:42:50 -0700

    Fixing heroku issues

--- sec/settings.py
+++ sec/settings.py
@@ -108,7 +108,7 @@ else:
 
             'ENGINE': 'django.db.backends.sqlite3',
 
-            'NAME': os.path.join(MAIN_DIR, 'db.sqlite3'),
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
         }
 
