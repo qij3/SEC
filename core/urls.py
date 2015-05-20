b41e96ecbd8f7c1d0fb1886d05f900f17476c3b4
qjin <qjin@mingyuedeMacBook-Air.local>
2015-05-20 00:11:18 -0700

    Permission add for unauthorized user

--- core/urls.py
+++ core/urls.py
@@ -2,22 +2,20 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 import core.views as coreviews
 
+from django.contrib.auth.decorators import login_required
+
 urlpatterns = patterns('',
 
  url(r'^$', coreviews.LandingView.as_view()),
  url(r'team/$', coreviews.TeamListView.as_view()),
  url(r'team/(?P<pk>\d+)/detail/$', coreviews.TeamDetailView.as_view(), name='team_list'),
- url(r'team/create/$', coreviews.TeamCreateView.as_view()),
- url(r'team/(?P<pk>\d+)/update/$', coreviews.TeamUpdateView.as_view(), name='team_update'),
+ url(r'team/create/$', login_required(coreviews.TeamCreateView.as_view())),
+ url(r'team/(?P<pk>\d+)/update/$', login_required(coreviews.TeamUpdateView.as_view()), name='team_update'),
 
  url(r'profile/$', coreviews.ProfileListView.as_view()),
  url(r'profile/(?P<pk>\d+)/detail/$', coreviews.ProfileDetailView.as_view(), name='profile_list'),
- url(r'profile/create/$', coreviews.ProfileCreateView.as_view()),
- url(r'profile/(?P<pk>\d+)/update/$', coreviews.ProfileUpdateView.as_view(), name='profile_update'),
-
-
- url(r'team/(?P<pk>\d+)/profile/create/$', coreviews.ProfileCreateView.as_view(), name='profile_create'),
- url(r'team/(?P<pk>\d+)/profile/update/$', coreviews.ProfileUpdateView.as_view(), name='profile_update'),
+ url(r'profile/create/$', login_required(coreviews.ProfileCreateView.as_view())),
+ url(r'profile/(?P<pk>\d+)/update/$', login_required(coreviews.ProfileUpdateView.as_view()), name='profile_update'),
 
 
  url(r'entrance/$', coreviews.entrance),
